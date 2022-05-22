e7a7e047bc5470a3673fd69e92410d411120b2a9
Charlie McMahon <charliekmcmahon@gmail.com>
2022-05-22 20:31:28 +1000

    complete rewrite

--- index.js
+++ index.js
@@ -4,139 +4,112 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const got = require('got');
-const Bottleneck = require("bottleneck");
+
 var mysql = require('mysql');
-var userIsAuthorised;
+var unirest = require('unirest');
 
 var dbUser = "netclaw";
-var dbPassword = "mJs2Hk6saSaT8gkE"; // You seriously suck if you try and breach our database.
-var dbHost = "netclaw-prod-db.mysql.database.azure.com";
+var dbPassword = "N3tcl@w123";
+var dbHost = "netclaw-dev-db.mysql.database.azure.com";
 
-var goUsers = [];
+var userAuthorised;
 
-// SQL Connection & Error check
+// Database connection
 var con = mysql.createConnection({
 	host: dbHost,
 	user: dbUser,
 	password: dbPassword,
-	database: "netclaw"
+	database: "netclaw-dev"
 });
 
 con.connect(function(err) {
 	if (err) {console.log(err)}
 	
-});
+}); console.log('Successfully connected to db.');
 
-console.log('Successfully connected to db.');
-
-function makeRequest(fullNodeURL, command) {
-	// Make the web reqest to the node
-	got.get(`${fullNodeURL}:8888/${command}`, { responseType: 'text' })
-		.then(res => {
-			console.log(`Request Sent! Response from server:`);
-		})
-		.catch(err => {
-			//console.log('Error: ', err.message);
-		});
-}
-
-function moveClaw(nodeURL, direction) {
-	// On a joystick / keyb button press, move the claw
-	const limiter = new Bottleneck({
-		minTime: 1000,
-		reservoir: 3,
-		strategy: Bottleneck.strategy.OVERFLOW // When the function is spammed for whatever reason, only 5 requests are executed per 200ms.
-	}); // Limit requests to once every 200ms
-	limiter.schedule(() => makeRequest(nodeURL, direction))
-		.then((result) => {
-		});
-
-}
+// Middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// API calls 
+
+///////////// --------- Requests ---------- //////////////
+
+function makeRequest(command, machine) {
+
+	// Get the machine's node URL
+	con.query("SELECT nodeURL, nodePort FROM machines WHERE id = '" + machine + "'", function (err, result, fields) {
+		if (err) {console.log(err)}
+
+		var nodeURL = result[0].nodeURL;
+		var nodePort = result[0].nodePort;
+
+		// Make the request
+		console.log('sending request...');
+
+		var nodeReq = unirest('POST', `http://${nodeURL}:${nodePort}/api`)
+		.headers({
+		'Content-Type': 'application/json'
+		})
+		.send(JSON.stringify({
+		"command": command
+		}))
+		.end(function (res) { 
+		if (res.error) throw new Error(res.error); 
+		console.log(res.raw_body);
+		});
+	
+	});
+}
+
+///////////// Api Call //////////////
+
 app.post('/api', function(req, res) {
 
-	//function apiAsync() {
-	reqMove = req.body.direction;
-	reqMachineID = req.body.machineID;
-	reqUsername = req.body.userID;
-	reqTime = new Date().toLocaleTimeString();
+	command = req.body.command;
+	machineID = req.body.machine;
+	nickname = req.body.user;
+	requestTime = new Date().toLocaleTimeString();
 
-	console.log("API Incoming =>", "command -", reqMove, "machine -", reqMachineID, "user_username -", reqUsername, ` @ ${reqTime}`);
-
-	con.query('SELECT `requestURL` FROM `machinestats` WHERE `id` = ' + reqMachineID, function(err, result) {
-		if (err) throw err;
-		if (result) {
-			console.log('Machine url = ' + (result[0].requestURL));
-			machineNodeURL = result[0].requestURL;
-		}
-		else {
-			// Machine
-			res.json({ code: "Error" });
-		}
-
-	});
-
-	con.query('SELECT * FROM `newqueue` WHERE `playing` = 1 AND `uuid` = ' + reqUsername, function(err, result) {
-		if (err) throw err;
-		if (result.length > 0) {
-			//console.log('Player is authorised');
-			userIsAuthorised = true;
-			checkUserAuth();
-		}
-		else if (result.length == 0) {
-			//console.log('Player is not authorised');
-			userIsAuthorised = false;
-		}
-		else {
-		}
-
-	});
-
-	function checkUserAuth() {
-
-		if (reqMove == "goReset" && goUsers[goUsers.length - 1] == reqUsername) {
-			// Reset the user's go
-			goUsers.push('reset');
-			console.log(goUsers);
-		}
-		else {
-			// Any other command
-		}
-
-		if (reqMove == "go" && goUsers[goUsers.length - 1] == reqUsername) {
-			console.log('User already pressed go');
-			userIsAuthorised = false;
-			console.log(goUsers);
-		}
-		else if (reqMove == "go") {
-			console.log("'go' authorised");
-			console.log(goUsers);
-			goUsers.push(reqUsername);
-			console.log(goUsers);
-		}
-		else {
-			// any other command
-		}
-
-		if (userIsAuthorised == true) {
-			// Send the request to the machine!
-			moveClaw(machineNodeURL, reqMove);
-			res.json({ code: "Success" });
-		}
-		else if (userIsAuthorised == false) {
-			res.json({ code: "Unauthorised" });
-		}
-		else {
-			res.json({ code: "Error" });
-		}
-	}
+	console.log(`${requestTime} - ${nickname} - ${machineID} - ${command}`);
 
 });
+
+	try {
+		// Check if user is currently in the currentlyplaying table
+		con.query(`SELECT * FROM currentlyplaying WHERE machineID = '${machineID}'`, function (err, result) {
+			if (err) throw err;
+			if (result.length > 0) {
+				dbNickname = result[0].userNickname;
+				console.log(`${nickname} is currently playing on ${machineID}`);
+				if (dbNickname == nickname) {
+					console.log('user is authorised');
+					userAuthorised = true;
+				}
+				else {
+					console.log('user is not authorised');
+					userAuthorised = false;
+				}
+			}
+		});
+
+	} catch (error) {
+		res.json({ code: "Error" });
+	}
+
+	if (userAuthorised == true) {
+		// Send the request
+		makeRequest(command, machineID);
+		res.json({ code: "Success" });
+	}
+	else if (userIsAuthorised == false) {
+		res.json({ code: "Unauthorised" });
+	}
+	else {
+		res.json({ code: "Error" });
+	}
+	
+
 
 app.get('/', function(req, res) {
 	res.send("Hello, World!");
@@ -146,11 +119,10 @@ app.get('/api/ping', function(req, res) {
 	res.send("pong!");
 });
 
-app.post('/apiCheck', function(req, res) {
+app.post('/api/check', function(req, res) {
 	console.log('API Checked! sending back 400.');
 	res.status(400);
 });
-
 
 app.listen(80);
 
